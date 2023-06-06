c5b0a42709ab50197a79a197f078bab0656f9447
Charlene Yang <8636796+cyanguwa@users.noreply.github.com>
2023-06-06 14:45:32 -0700

    revert changes in cmake list and copyright checker
    Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- qa/L0_license/copyright_checker.py
+++ qa/L0_license/copyright_checker.py
@@ -70,7 +70,6 @@ def get_file_type(path):
            "cfg": ["cfg"],
            "sh":  ["sh"],
            "md":  ["md"],
-           "cmake": ["cmake"]
           }
     tmp = path.split(".")
     for filetype, ext_list in ext.items():
