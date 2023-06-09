d31f2b63efca5079b104cfa15c045b40a95eeee1
Charlene Yang <8636796+cyanguwa@users.noreply.github.com>
2023-06-08 23:50:59 -0700

    disable fused attn when padding and mask is not none
    Signed-off-by: Charlene Yang <8636796+cyanguwa@users.noreply.github.com>

--- transformer_engine/pytorch/attention.py
+++ transformer_engine/pytorch/attention.py
@@ -879,6 +879,7 @@ class DotProductAttention(torch.nn.Module):
 
         if self.attn_mask_type == "padding" and attention_mask is not None:
             use_flash_attention = False
+            use_fused_attention = False
 
         if is_in_onnx_export_mode():
             use_flash_attention = False
